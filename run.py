6ed4d6bed56b4a550011f167eb151b9453d4664e
Xander <xander@axiom-of-joy.com>
2020-06-20 20:53:28 -0700

    Initial commit.

---
+++ run.py
@@ -0,0 +1,8 @@
+from tensorflow.keras.applications import efficientnet
+import tensorflow.keras as keras
+
+#model = efficientnet.EfficientNetB7()
+#preds = model.predict(efficientnet.preprocess(image))
+#print(efficientnet.decode_predictions(preds))
+
+print('Hello world')
